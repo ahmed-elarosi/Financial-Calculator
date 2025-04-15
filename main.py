472d0b0d6d0e1f1c3c435937e021ba0521b5b6a1
Ahmed-Elarosi <cabry95@protonmail.com>
2025-04-15 07:40:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def calculate_finances(monthly_income: float, tax_rate: float, currency: str) -> None:
+    monthly_tax: float = monthly_income * (tax_rate / 100)
+    monthly_net_income: float = monthly_income - monthly_tax
+    yearly_salary: float = monthly_income * 12
+    yearly_tax: float = monthly_tax * 12
+    yearly_net_income: float = yearly_salary - yearly_tax
+
+    print("-------------------------------------------------------------")
+    print(f"Monthly Income: {currency}{monthly_income:,.2f}")
+    print(f"Tax Rate: {tax_rate:,.0f}%")
+    print(f"Monthly Tax: {currency}{monthly_tax:,.2f}")
+    print(f"Monthly Net Income: {currency}{monthly_net_income:,.2f}")
+    print(f"Yearly Income: {currency}{yearly_salary:,.2f}")
+    print(f"Yearly Tax: {currency}{yearly_tax:,.2f}")
+    print(f"Yearly Net Income: {currency}{yearly_net_income:,.2f}")
+    print("-------------------------------------------------------------")
+
+
+calculate_finances(1550, 20, "€")
