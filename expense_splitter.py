faa5ef1191e5a8f08b3c6546848f9bf67b2bddf7
Ahmed-Elarosi <cabry95@protonmail.com>
2025-04-26 15:24:30 +0200

    add calculat func

--- expense_splitter.py
+++ expense_splitter.py
@@ -1,6 +1,11 @@
 def calculate_split(total_amount: float, number_of_people: int, currency: str) -> None:
     if number_of_people < 1:
         raise ValueError("Dude you have to pay for yourself :)")
+    share_per_person: float = total_amount / number_of_people
+
+    print(f"Total Expenses: {currency}{total_amount:,.2f}")
+    print(f"Number Of People: {number_of_people}")
+    print(f"Each Person Should Pay: {currency}{share_per_person:,.2f}")
 
 
 def main(): ...
