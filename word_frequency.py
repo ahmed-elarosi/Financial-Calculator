c2e6bb0a6fda56a284715ccaa29c9ac3ef49a041
Ahmed-Elarosi <cabry95@protonmail.com>
2025-05-02 14:25:27 +0200

    add word counter

--- word_frequency.py
+++ word_frequency.py
@@ -4,4 +4,21 @@ import re
 
 def get_frequency(text: str) -> list[tuple[str, int]]:
     lowered_text: str = text.lower()
-    words:list[str]: = re.findall(r"")
+    words: list[str] = re.findall(r"\b\w+\b", lowered_text)
+    word_counts: Counter = Counter(words)
+    number_of_words: int = len(lowered_text.split())
+
+    print(f"Number of Words in this Text are: {number_of_words} ")
+    print(60 * "-")
+    return word_counts.most_common()
+
+
+def main():
+    text: str = input("Enter Your Text: ").strip()
+    word_frequencies: list[tuple[str, int]] = get_frequency(text)
+    for word, count in word_frequencies:
+        print(f"{word}: {count} ")
+
+
+if __name__ == "__main__":
+    main()
